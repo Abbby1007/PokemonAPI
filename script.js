036a1c5d2953dfa4229e0aac1eb3a0779a0997e8
Abbby1007 <Abigailloken25@gmail.com>
2026-06-15 16:31:13 -0400

    Code ot fetch javascript data

--- script.js
+++ script.js
@@ -6,16 +6,29 @@ and used for interacting with APIs to retrieve
 and send data asynchronously over the web.
 fetch(url, {options})
 */
+fetchData();
 
-console
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
-    .then(response => {
+async function fetchData(){
+
+
+    try{
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();7
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
 
         if(!response.ok){
-           throw new Error("Could not fetch resource"); 
+            throw new Error("Could not fetch resource");
         }
-        return response.json();
-    })
 
-    .then(data => console.log(data.name))
-    .catch(error => console.error(error));
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById(`pokemonSprite`);
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    }
+
+    catch(error){
+        console.error(error);
+    }
+}
