6d7b309339d2a03e83b1b8c661ac4b143490248e
Abbby1007 <Abigailloken25@gmail.com>
2026-06-15 14:08:16 -0400

    Basic fetch code

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-// fetch = Function use for making HTTP requests to fetch resources. (Json)
-fetchData();
+/*
+fetch = Function use for making HTTP requests to 
+fetch resources. (JSON style data, images, files)
+simplifies asynchronous data fetching in JavaScript 
+and used for interacting with APIs to retrieve
+and send data asynchronously over the web.
+fetch(url, {options})
+*/
+
+console
+fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+    .then(response => {
+
+        if(!response.ok){
+           throw new Error("Could not fetch resource"); 
+        }
+        return response.json();
+    })
+
+    .then(data => console.log(data.name))
+    .catch(error => console.error(error));
