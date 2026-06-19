8c1ea1796e6e321c2761bc44bf2d57afc5905ae9
Abbby1007 <Abigailloken25@gmail.com>
2026-06-19 10:21:17 +0900

    Add an ability Page

---
+++ script1.js
@@ -0,0 +1,105 @@
+/*
+fetch = Function use for making HTTP requests to 
+fetch resources. (JSON style data, images, files)
+simplifies asynchronous data fetching in JavaScript 
+and used for interacting with APIs to retrieve
+and send data asynchronously over the web.
+fetch(url, {options})
+*/
+// function to load up the pokemon image
+// let nextQuestion = 0;
+let numQuestion = 0;
+let randomNum;
+let correct = 0;
+
+// SCRIPT FOR THE NAME QUIZZ
+
+// User will start the game, this will trigger the first question
+
+// Then user will get question, the next button will disappear. 
+// once user answers the question then the next button will come back
+    // whether or not the user is correct or wrong
+
+
+async function loadImage(){
+    if(numQuestion === 10){
+        document.getElementById("number").innerHTML = `<span id="done">DONE GAME <br> You have ${correct} point(s)<br> You can try again or go back to the Home Page</span>`;
+          document.getElementById("next").style.visibility = "hidden";
+ document.getElementById("tryagain").style.visibility = "visible";
+ document.getElementById("home").style.visibility = "visible";
+          return;
+    } 
+    // make the next button question disappear
+     document.getElementById("tryagain").style.visibility = "hidden";
+     document.getElementById("home").style.visibility = "hidden";
+    document.getElementById("start").style.display = `none`;
+    document.getElementById("first").style.display = `block`;
+    document.getElementById("next").style.visibility = "hidden";
+    document.getElementById("check").style.visibility = "visible";
+
+    numQuestion += 1;
+    document.getElementById("question").innerHTML = `<h2 id="question">Question ${numQuestion}/10 <br> <span id="instructions"> Type the name of the Pokemon shown below </span> <br> ${correct}/10 points</h2>`
+    //Generate random number from 0 to 300
+    try{
+        randomNum = Math.floor(Math.random() * 301);
+
+       
+
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`);
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+    // GENERATE THE SPRITE
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const dataName = data.name;
+         document.getElementById("number").innerHTML = `${dataName}`;
+        const imgElement = document.getElementById(`pokemonSprite`);
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+    }
+       catch(error){
+        console.error(error);
+    }
+
+document.getElementById("check").innerHTML = `<button onclick="checkAnswer(randomNum)">CHECK</button>`;
+   
+  }     
+async function checkAnswer(num){
+    try{
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
+            const data = await response.json();
+            const dataName = data.name;
+
+        if(!response.ok){
+            throw new Error("Could not fetch resource");
+        }
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        //if the inputed pokemon name is equal to the name of the id pokemon
+            // then tell the user they are correct
+        // if not 
+            // then tell the user they are wrong
+        if(pokemonName === dataName){
+            document.getElementById("number").innerHTML=`<span id="correct";'>CORECCTTT!!!! <br> +1 point</span>`;
+             document.getElementById("next").style.visibility = "visible";
+                document.getElementById("check").style.visibility = "hidden";
+                correct += 1;
+
+        }
+        else if (pokemonName != dataName){
+            document.getElementById("number").innerHTML=`<span id="wrong">Wrong :( <br> The correct answer is "${dataName}"</span>`;  
+            document.getElementById("next").style.visibility = "visible";
+            document.getElementById("check").style.visibility = "hidden";
+        }
+    }
+       catch(error){
+        console.error(error);
+    }
+    document.getElementById("question").innerHTML = `<h2 id="question">Question ${numQuestion}/10 <br> <span id="instructions"> Type the name of the Pokemon shown below </span> <br> ${correct}/10 points</h2>`
+}
+
+
+
+
