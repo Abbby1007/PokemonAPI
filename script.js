287ec86bfd6ece2f652d8b77095238f20969b15c
Abbby1007 <Abigailloken25@gmail.com>
2026-06-19 09:24:08 +0900

    added styling, tryagain and home

--- script.js
+++ script.js
@@ -19,20 +19,26 @@ let correct = 0;
 // Then user will get question, the next button will disappear. 
 // once user answers the question then the next button will come back
     // whether or not the user is correct or wrong
+
+
 async function loadImage(){
     if(numQuestion === 10){
-        document.getElementById("number").innerHTML = `DONE GAME`;
-        return;
+        document.getElementById("number").innerHTML = `<span id="done">DONE GAME <br> You have ${correct} point(s)<br> You can try again or go back to the Home Page</span>`;
           document.getElementById("next").style.visibility = "hidden";
+ document.getElementById("tryagain").style.visibility = "visible";
+ document.getElementById("home").style.visibility = "visible";
+          return;
     } 
     // make the next button question disappear
-    document.getElementById("start").style.visibility = "hidden";
+     document.getElementById("tryagain").style.visibility = "hidden";
+     document.getElementById("home").style.visibility = "hidden";
+    document.getElementById("start").style.display = `none`;
     document.getElementById("first").style.display = `block`;
     document.getElementById("next").style.visibility = "hidden";
     document.getElementById("check").style.visibility = "visible";
 
     numQuestion += 1;
-    document.getElementById("question").innerHTML = `<h2 id="question">Question ${numQuestion}/10 <br> type the answer for what pokemon is shown <br> ${correct}/10 points</h2>`
+    document.getElementById("question").innerHTML = `<h2 id="question">Question ${numQuestion}/10 <br> <span id="instructions"> Type the name of the Pokemon shown below </span> <br> ${correct}/10 points</h2>`
     //Generate random number from 0 to 300
     try{
         randomNum = Math.floor(Math.random() * 301);
@@ -76,14 +82,14 @@ async function checkAnswer(num){
         // if not 
             // then tell the user they are wrong
         if(pokemonName === dataName){
-            document.getElementById("number").innerHTML=`<p>CORECCTTT!!!!</p>`;
+            document.getElementById("number").innerHTML=`<span id="correct";'>CORECCTTT!!!! <br> +1 point</span>`;
              document.getElementById("next").style.visibility = "visible";
                 document.getElementById("check").style.visibility = "hidden";
                 correct += 1;
 
         }
         else if (pokemonName != dataName){
-            document.getElementById("number").innerHTML=`<p>Wrong :( <br> The correct answer is "${dataName}"</p>`;  
+            document.getElementById("number").innerHTML=`<span id="wrong">Wrong :( <br> The correct answer is "${dataName}"</span>`;  
             document.getElementById("next").style.visibility = "visible";
             document.getElementById("check").style.visibility = "hidden";
         }
@@ -91,37 +97,9 @@ async function checkAnswer(num){
        catch(error){
         console.error(error);
     }
-    document.getElementById("question").innerHTML = `<h2 id="question">Question ${numQuestion}/10 <br> type the answer for what pokemon is shown <br> ${correct}/10 points</h2>`
+    document.getElementById("question").innerHTML = `<h2 id="question">Question ${numQuestion}/10 <br> <span id="instructions"> Type the name of the Pokemon shown below </span> <br> ${correct}/10 points</h2>`
 }
 
 
 
 
-async function fetchData(){
-
-
-
-
-    try{
-        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-
-        if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
-
-        const data = await response.json();
-        const pokemonSprite = data.sprites.front_default;
-        const imgElement = document.getElementById(`pokemonSprite`);
-        const number = data.id;
-       document.getElementById("number").innerHTML = `${number}`
-
-        imgElement.src = pokemonSprite;
-        imgElement.style.display = "block";
-    }
-
-    catch(error){
-        console.error(error);
-    }
-}
